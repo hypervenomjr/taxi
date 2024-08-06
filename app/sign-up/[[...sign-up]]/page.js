e885cd7221858917cb25e51b8de36d7becfed9b7
hypervenomjr <165468552+hypervenomjr@users.noreply.github.com>
2024-08-06 15:19:04 +0530

    Update page.js
    updates signup page

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -2,5 +2,14 @@ import { SignedOut, SignUp } from "@clerk/nextjs";
 
 export default function Page(){
 
-    return <SignUp></SignUp>
+   return(
+    <div>
+    <Image src='/uberbanner.jpg' width={900} height={800} className=" object-contain h-full w-full"></Image>
+<div className=' absolute right-5 top-24'> 
+<SignUp />
+
+</div>
+
+</div>
+    )
 }
